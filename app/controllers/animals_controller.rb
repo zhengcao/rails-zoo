fc65d8a9cbdd50a4a196c71de44a5b359d2254ac
Zheng Cao <chori84@gmail.com>
2018-03-26 15:00:16 -0700

    Finish creating an animal

--- app/controllers/animals_controller.rb
+++ app/controllers/animals_controller.rb
@@ -8,9 +8,16 @@ class AnimalsController < ApplicationController
   end
 
   def new
+    @animal = Animal.new
   end
 
   def create
+    @animal = Animal.new(animal_params)
+    if @animal.save
+      redirect_to animals_path
+    else
+      render :new
+    end
   end
 
   def edit
@@ -20,5 +27,11 @@ class AnimalsController < ApplicationController
   end
 
   def destroy
+  end
+
+  private
+
+  def animal_params
+    return params.require(:animal).permit(:name, :species, :age)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,11 +3,10 @@ Rails.application.routes.draw do
   root 'animals#index'
 
   get 'animals', to: 'animals#index', as: 'animals'
+  get 'animals/new', to: 'animals#new', as: 'new_animal'
+  post 'animals', to: 'animals#create'
   get 'animals/:id', to: 'animals#show', as: 'animal'
 
-  get 'animals/new'
-
-  get 'animals/create'
 
   get 'animals/edit'
 
