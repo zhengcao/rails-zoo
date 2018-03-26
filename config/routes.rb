66131b6d4239c38f057d3c20b7e105de48c5965c
Zheng Cao <chori84@gmail.com>
2018-03-26 11:32:07 -0700

    Generate controller Animals

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,17 @@
 Rails.application.routes.draw do
+  get 'animals/index'
+
+  get 'animals/show'
+
+  get 'animals/new'
+
+  get 'animals/create'
+
+  get 'animals/edit'
+
+  get 'animals/update'
+
+  get 'animals/destroy'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
